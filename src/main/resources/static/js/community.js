beb9b0622e29903ae17bb5e48fb4a66f238000d0
we5t email = ver-tigo@foxmail.com <ver-tigo@foxmial.com>
2019-11-13 11:03:36 +0800

    add login state check

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -11,8 +11,17 @@ function post() {
             "type": 1
         }),
         success: function (response) {
-            if(response.code != 200)
+            if (response.code != 200)
                 alert(response.message);
+            else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=Iv1.628b4a885a01a11f&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                }
+            }
         },
         dataType: "json"
     });
